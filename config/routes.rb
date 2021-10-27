7d42316c9ce96fb3a66270bd1cd4c699f420a273
Tolibjon <tolibjohn@gmail.com>
2021-10-27 21:15:58 +0500

    Create views for Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  get 'users/index'
-  get 'users/show'
+  resources :users, only:[:index, :show] do
+    resources :posts, only:[:index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
