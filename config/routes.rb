40e3f59eb7fc202f805df3f60fcfcb746b514dd2
Tolibjon <tolibjohn@gmail.com>
2021-10-27 21:03:18 +0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
+  get 'users/index'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
