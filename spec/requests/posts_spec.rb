d97a3fb483dc928334930181582edead3e67e529
Tolibjon <tolibjohn@gmail.com>
2021-10-29 08:52:52 +0500

    Add tests for actions of Posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get user_posts_path(1) }
+
+    it 'should have correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Here is a list of posts for given user')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get user_post_path(1, 1) }
+
+    it 'should have correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Here is a post for given user with given id of post')
+    end
   end
 end
