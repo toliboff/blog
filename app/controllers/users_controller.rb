6bc850cf460fdf77446eb4251bde6247b17804a8
Tolibjon <tolibjohn@gmail.com>
2021-11-10 20:54:16 +0500

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_posts = Post.where(author_id: @user.id)
-    @comments = Comment.all;
+    @comments = Comment.all
   end
 end
