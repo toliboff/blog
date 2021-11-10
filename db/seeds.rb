88c2437b4c1a779906552555e9f5afc719900416
Tolibjon <tolibjohn@gmail.com>
2021-11-10 16:03:21 +0500

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+random = Random.new
+user_qty = random.rand(3..10)
+
+user_qty.times do |i|
+	user = User.create(name: "User#{i+1}", bio: "A bio of User#{i+1}.")
+	post_qty = random.rand(2..5)
+
+	post_qty.times do |j|
+		post = Post.create(title: "Post title #{j+1}", text: "Post text goes here", author: user)
+		
+		comment_qty = random.rand(20)
+		comment_qty.times do |j|
+			Comment.create(text: "Comment ##{j+1}", post: post, author: user)
+		end
+	end
+end
