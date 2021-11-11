a10a7b155bbd4edf48b6c14460d6e6d67140b86c
Tolibjon <tolibjohn@gmail.com>
2021-11-11 21:59:04 +0500

    Add routes for Likes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
       resources :comments, only: %i[new create]
     end
   end
+
+  resources :likes, only: %i[create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
