0ed7fdbaaca42ef468768922970553a798c6532a
Tolibjon <tolibjohn@gmail.com>
2021-11-11 11:16:50 +0500

    Add routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # Controller for users
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.order('id')
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
