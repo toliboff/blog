71b69a35caa81f8a5d998b24fef7f256d81eb306
Tolibjon <tolibjohn@gmail.com>
2021-11-11 11:16:27 +0500

    Add actions to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,4 +14,24 @@ class PostsController < ApplicationController
     @users = User.all
     @likes = @post.likes.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    @post.author = current_user 
+    if @post.save
+      redirect_to user_posts_path
+    else 
+      render :new
+    end
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
