780e5efc5043231315258bcd90cf99608774ead7
Tolibjon <tolibjohn@gmail.com>
2021-11-12 10:47:42 +0500

    Add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy, foreign_key: 'author_id'
   has_many :comments, dependent: :destroy, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   def recent_posts
     posts.last(3)
   end
