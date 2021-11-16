76615ca7674c568da644b064d07f41ce6ab6750b
Tolibjon <tolibjohn@gmail.com>
2021-11-16 18:28:19 +0500

    Add permissions for current user and for admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+      if user.role == 'admin'
+        can :manage, :all
+      else
+        can :manage, Post, author_id: user.id
+        can :read, Post
+      end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
