de4442b6cc0a70cdc83b8c30ee5ccfa7e6793dab
m05e5 <calebmoses1111@gmail.com>
2021-11-18 23:49:46 +0100

    add test for number of comments, first comment, number of likes

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -41,5 +41,26 @@ RSpec.describe 'Post', type: :system do
     it 'I can see some of the post\'s body.' do
       expect(page).to have_content('1Lorem ipsum dolor set amet.')
     end
+
+    it 'I can see the first comments on a post.' do
+      expect(page).to have_content('Random comment')
+    end
+
+    it 'I can see how many comments a post has.' do
+      expect(page).to have_content('Comments: 2')
+    end
+
+    it 'I can see how many likes a post has.' do
+      expect(page).to have_content('Likes: 0')
+    end
+
+    it 'I can see a section for pagination if there are more posts than fit on the view.' do
+      expect(page).to have_button('Pagination')
+    end
+
+    it 'When I click on a post, it redirects me to that post\'s show page.' do
+      click_link 'Post #1'
+      expect(page).to have_current_path(user_post_path(@tolib.id, @tolib.posts.first.id))
+    end
   end
 end
